ecc082762bfe7a140ef9f07db579fa05c52741d6
javierhernandezprogramador-stack <javier.hernandez.programador@gmail.com>
2026-08-26 00:01:23 -0600

    chore: agregue loader a los modulos

--- themes/web/assets/js/app.js
+++ themes/web/assets/js/app.js
@@ -30,3 +30,57 @@ document.addEventListener('DOMContentLoaded', function () {
     if (closeBtn) closeBtn.addEventListener('click', closeSidebar);
     if (overlay) overlay.addEventListener('click', closeSidebar);
 });
+
+function toggleScrolled() {
+    const selectBody = document.querySelector('body');
+    const selectHeader = document.querySelector('#header');
+    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
+    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+}
+
+(function () {
+
+    const originalOpen = XMLHttpRequest.prototype.open;
+    const originalSend = XMLHttpRequest.prototype.send;
+
+    let requests = 0;
+
+    XMLHttpRequest.prototype.open = function (method, url) {
+        this._url = url;
+
+        return originalOpen.apply(this, arguments);
+    };
+
+    XMLHttpRequest.prototype.send = function () {
+
+        requests++;
+
+        mostrarLoader();
+
+        this.addEventListener('loadend', function () {
+
+            requests--;
+
+            if (requests <= 0) {
+                requests = 0;
+                ocultarLoader();
+            }
+
+        });
+
+        return originalSend.apply(this, arguments);
+    };
+
+    function mostrarLoader() {
+        document.getElementById('loader').classList.remove('ocultar');
+        document.getElementById('loader').classList.add('mostrar');
+    }
+
+    function ocultarLoader() {
+        document.getElementById('loader').classList.remove('mostrar');
+        document.getElementById('loader').classList.add('ocultar');
+    }
+
+})();
+
+
